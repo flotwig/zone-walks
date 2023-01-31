43605d2e81725d9215f4640f61a66f415fc354db
Zach Bloomquist <git@chary.us>
2023-01-30 19:52:43 -0500

    Add TLD ignore list

--- build-walkable-zones.sh
+++ build-walkable-zones.sh
@@ -3,4 +3,5 @@
 curl https://raw.githubusercontent.com/flotwig/monoidic-TLDR2/master/walkable_zones.md | \
     cut -d "\`" -f2 -s - | \
     cut -d "." -f1 | \
+    grep -v -f ./ignore-tld-list.txt | \
     jq -nR '{ zones: [ inputs | split("\n") | add | { zone: strings }] }' > ./walkable-zones.json
