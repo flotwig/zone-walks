b79482ed3d15c3ea20fa37b73143a8d6d60be7f7
Zach Bloomquist <git@chary.us>
2023-02-02 23:25:15 -0500

    Fix stats punycoding

--- build-stats.sh
+++ build-stats.sh
@@ -7,7 +7,7 @@ WC=$(head -n-1 <<<"$WC" | sort -rbn)
 
 echo "$WC" | \
     jq --arg TOTAL $TOTAL -nR '{
-        zones: [ inputs | capture( " *(?<domains>[0-9]+) +./lists/(?<zone>[a-z-]+)" ) ],
+        zones: [ inputs | capture( " *(?<domains>[0-9]+) +./lists/(?<zone>[a-z0-9-]+)" ) ],
         total: $TOTAL
     }' > ./stats.json
 
