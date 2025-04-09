14a3468a3d512e4d9b33762282d2124e60db9f49
Zach Bloomquist <git@chary.us>
2025-04-09 02:15:40 -0400

    Update build-walkable-zones.sh

--- build-walkable-zones.sh
+++ build-walkable-zones.sh
@@ -4,7 +4,7 @@ set +e
 curl https://raw.githubusercontent.com/flotwig/monoidic-TLDR2/master/walkable_zones.md | \
     cut -d "\`" -f2 -s - | \
     cut -d "." -f1 | \
-    grep -v -f ./ignore-tld-list.txt | \
+    #grep -v -f ./ignore-tld-list.txt | \
     jq -nR '{ zones: [ inputs | split("\n") | add | { zone: strings }] }' > ./walkable-zones.json
 
 chevron -l "[[" -r "]]" -d ./walkable-zones.json ./.github/workflows/walk.yaml.template > ./.github/workflows/walk.yaml
